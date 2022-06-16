9113e8b7db40971100ffbc42ad4a3e3a4f2d6464
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-06-15 20:19:16 -0600

    Correct the source file name

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { OutLink } from "./components/OutLink";
 import "./App.css";
 
 export function App() {
+	const thisFile = "src/App.tsx";
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<Logo />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					Edit <code>{thisFile}</code> and save to reload.
 				</p>
 				<OutLink href="https://reactjs.org">Learn React</OutLink>
 			</header>
